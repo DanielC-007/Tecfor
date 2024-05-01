e70a6f455e0cf5b8d35e1d60afde2a5d36333ec1
MrMeme777 <brz.danielcarlos@gmail.com>
2024-05-01 12:38:14 -0300

    closeNav

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,10 @@
 console.log("Olá, tudo certo");
+
+function navOpenClose() {
+    var navSalas = document.getElementById("salas");
+    if (navSalas.style.display === "none" || navSalas.style.display === "") {
+        navSalas.style.display = "block";
+    } else {
+        navSalas.style.display = "none";
+    }
+}
