2ab6fc3913d0556ac1c978a63299ba553e4c241e
MrMeme777 <brz.danielcarlos@gmail.com>
2024-05-10 15:46:12 -0300

    userAside-someJS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,3 +8,22 @@ function navOpenClose() {
         navSalas.style.display = "none";
     }
 }
+
+function confirmarConta() {
+    var nome = document.getElementById("nameUser");
+    var email = document.getElementById("emailUser");
+    var pass = document.getElementById("passUser");
+    if (nome && email && pass == false) {
+        alert("Preencha todos os campos!");
+    }
+}
+
+function exibirSenha() {
+    var visualizar = document.getElementById("passUser");
+    if (visualizar.type === "password") {
+        visualizar.type = "text";
+    }
+    else {
+        visualizar.type = "password";
+    }
+}
