c8d81e707f31047e13eed316f883909fcc61a234
DanielC-007 <brz.danielcarlos@gmail.com>
2024-05-14 10:36:07 -0300

    CloseNav

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,36 +1,34 @@
 console.log("Olá, tudo certo");
 
 function navOpenClose() {
-    var navSalas = document.getElementById("salas");
-    var divclose = document.querySelector(".closeNav");
-    navSalas.classList.toggle("closed");
-    divclose.classList.toggle("closed");
-    if (navSalas.style.display === "none" || navSalas.style.display === "") {
-        navSalas.style.transform = "tranlateX(-100%)";
-        setTimeout(()=>{
-            navSalas.style.display = "none";
-        }, 500);
-    } else {
-        navSalas.style.display = "block";
-    }
+  var navSalas = document.getElementById("salas");
+  var bntClose = document.getElementsByClassName("closeNav");
+  if (
+    navSalas.style.transform === "translateX(0px)" ||
+    navSalas.style.transform === ""
+  ) {
+    navSalas.style.transform = "translateX(-400px)";
+    bntClose.style.transform = "translateX(0px)";
+  } else {
+    navSalas.style.transform = "translateX(0px)";
+    bntClose.style.transform = "translateX(450px)";
+  }
 }
 
-function confirmarConta() {
-    var nome = document.getElementById("nameUser");
-    var email = document.getElementById("emailUser");
-    var pass = document.getElementById("passUser");
-    if (nome && email && pass == false) {
-        alert("Preencha todos os campos!");
-    }
-}
+// function confirmarConta() {
+//   var nome = document.getElementById("nameUser");
+//   var email = document.getElementById("emailUser");
+//   var pass = document.getElementById("passUser");
+//   if (nome && email && pass == false) {
+//     alert("Preencha todos os campos!");
+//   }
+// }
 
 function exibirSenha() {
-    var visualizar = document.getElementById("passUser");
-    if (visualizar.type === "password") {
-        visualizar.type = "text";
-    }
-    else {
-        visualizar.type = "password";
-    }
+  var visualizar = document.getElementById("passUser");
+  if (visualizar.type === "password") {
+    visualizar.type = "text";
+  } else {
+    visualizar.type = "password";
+  }
 }
-
