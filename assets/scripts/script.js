8d49a7be44727c7f3b93a7e30554925b05676103
MrMeme777 <brz.danielcarlos@gmail.com>
2024-05-22 18:44:51 -0300

    update 05/22 18.45

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,27 +2,15 @@ console.log("Olá, tudo certo");
 
 function navOpenClose() {
   var navSalas = document.getElementById("salas");
-  var subContainer = document.getElementById("subContainer");
-  if (
-    navSalas.style.transform === "translateX(0px)" ||
-    navSalas.style.transform === ""
-  ) {
-    navSalas.style.transform = "translateX(-100%)";
-    subContainer.style.width = "100vw";
+  var container = document.getElementById("container");
+  if (navSalas.style.visibility === "hidden" || navSalas.style.visibility === ""){
+    navSalas.style.visibility = "hidden";
+    container.style.width = "100vw";
   } else {
-    navSalas.style.transform = "translateX(0px)";
-    subContainer.style.width = "calc(100vw - 300px)";
+    navSalas.style.visibility = "visible";
+    container.style.width = "calc(100vw - 300px)";
   }
 }
-
-// function confirmarConta() {
-//   var nome = document.getElementById("nameUser");
-//   var email = document.getElementById("emailUser");
-//   var pass = document.getElementById("passUser");
-//   if (nome && email && pass == false) {
-//     alert("Preencha todos os campos!");
-//   }
-// }
 
 function exibirSenha() {
   var visualizar = document.getElementById("passUser");
