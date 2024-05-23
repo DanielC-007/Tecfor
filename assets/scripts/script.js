22c681115153457411b0d47eb445d7f9bea27275
MrMeme777 <brz.danielcarlos@gmail.com>
2024-05-23 20:07:58 -0300

    update Theme 05/23 20.08

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,3 +20,41 @@ function exibirSenha() {
     visualizar.type = "password";
   }
 }
+
+function tema() {
+  const body = document.body;
+  if (body.classList.contains("lightTheme")) {
+    body.classList.remove("lightTheme");
+    updateImages('dark');
+  } else {
+    body.classList.add("lightTheme");
+    updateImages('light');
+  }
+}
+
+function updateImages(theme) {
+  const imageElements = [
+    {
+      id: "userImage",
+      dark: "public/imgs/userDark.png",
+      light: "public/imgs/userLight.png",
+    },
+    {
+      id: "homeImage",
+      dark: "public/imgs/homeDark.png",
+      light: "public/imgs/homeLight.png",
+    },
+    {
+      id: "sunImage",
+      dark: "public/imgs/sun.png",
+      light: "public/imgs/moon.png",
+    },
+  ];
+
+  imageElements.forEach((image) => {
+    const imgElement = document.getElementById(image.id);
+    if (imgElement) {
+      imgElement.src = theme === "dark" ? image.dark : image.light;
+    }
+  });
+}
