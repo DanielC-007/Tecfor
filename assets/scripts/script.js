0ec8d9c125b60d1d8dcae3041aff54dccf3d2f84
MrMeme777 <brz.danielcarlos@gmail.com>
2024-05-25 17:30:33 -0300

    update 05/25 17.30

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -29,24 +29,18 @@ function tema() {
   var hamburger = document.getElementById("hamburger");
   var user = document.getElementById("user");
   var sunMoon = document.getElementById("sunMoon");
-  var home = document.getElementById("home");
-  var chatImg = document.getElementsByClassName("chatImg");
 
   if (body.classList.contains("lightTheme")) {
     body.classList.remove("lightTheme");
     hamburger.src = "../public/imgs/hamburgerDark.png";
     user.src = "../public/imgs/userDark.png";
     sunMoon.src = "../public/imgs/sun.png";
-    home.src = "../public/imgs/homeDark.png";
-    chatImg.src = "../public/imgs/chatDark.png";
     localStorage.setItem("theme", "dark");
   } else {
     body.classList.add("lightTheme");
     hamburger.src = "../public/imgs/hamburgerLight.png";
     user.src = "../public/imgs/userLight.png";
     sunMoon.src = "../public/imgs/moon.png";
-    home.src = "../public/imgs/homeLight.png";
-    chatImg.src = "../public/imgs/chatLight.png";
     localStorage.setItem("theme", "light");
   }
 }
@@ -56,8 +50,6 @@ function carregarTema() {
   var hamburger = document.getElementById("hamburger");
   var user = document.getElementById("user");
   var sunMoon = document.getElementById("sunMoon");
-  var home = document.getElementById("home");
-  var chatImg = document.getElementsByClassName("chatImg");
 
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "light") {
@@ -65,15 +57,11 @@ function carregarTema() {
     hamburger.src = "../public/imgs/hamburgerLight.png";
     user.src = "../public/imgs/userLight.png";
     sunMoon.src = "../public/imgs/moon.png";
-    home.src = "../public/imgs/homeLight.png";
-    chatImg.src = "../public/imgs/chatLight.png";
   } else {
     body.classList.remove("lightTheme");
     hamburger.src = "../public/imgs/hamburgerDark.png";
     user.src = "../public/imgs/userDark.png";
     sunMoon.src = "../public/imgs/sun.png";
-    home.src = "../public/imgs/homeDark.png";
-    chatImg.src = "../public/imgs/chatDark.png";
   }
 }
 document.addEventListener("DOMContentLoaded", carregarTema);
