47a7972bbd1bccbec2fe29def8f3d037da05357d
MrMeme777 <brz.danielcarlos@gmail.com>
2024-05-26 16:29:37 -0300

    update loginPage 05/26 16.30

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -19,10 +19,13 @@ function navOpenClose() {
 
 function exibirSenha() {
   var visualizar = document.getElementById("passUser");
+  var eye = document.getElementById("eye");
   if (visualizar.type === "password") {
     visualizar.type = "text";
+    eye.src = "../public/imgs/closeEyeLight.png";
   } else {
     visualizar.type = "password";
+    eye.src = "../public/imgs/openEyeLight.png";
   }
 }
 
@@ -31,18 +34,21 @@ function tema() {
   var hamburger = document.getElementById("hamburger");
   var user = document.getElementById("user");
   var sunMoon = document.getElementById("sunMoon");
+  var eye = document.getElementById("eye");
 
   if (body.classList.contains("lightTheme")) {
     body.classList.remove("lightTheme");
     hamburger.src = "../public/imgs/hamburgerDark.png";
     user.src = "../public/imgs/userDark.png";
     sunMoon.src = "../public/imgs/sun.png";
+    eye.src = "../public/imgs/openEyeDark.png";
     localStorage.setItem("theme", "dark");
   } else {
     body.classList.add("lightTheme");
     hamburger.src = "../public/imgs/hamburgerLight.png";
     user.src = "../public/imgs/userLight.png";
     sunMoon.src = "../public/imgs/moon.png";
+    eye.src = "../public/imgs/openEyeLight.png";
     localStorage.setItem("theme", "light");
   }
 }
@@ -52,18 +58,20 @@ function carregarTema() {
   var hamburger = document.getElementById("hamburger");
   var user = document.getElementById("user");
   var sunMoon = document.getElementById("sunMoon");
-
+  var eye = document.getElementById("eye");
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "light") {
     body.classList.add("lightTheme");
     hamburger.src = "../public/imgs/hamburgerLight.png";
     user.src = "../public/imgs/userLight.png";
     sunMoon.src = "../public/imgs/moon.png";
+    eye.src = "../public/imgs/openEyeLight.png";
   } else {
     body.classList.remove("lightTheme");
     hamburger.src = "../public/imgs/hamburgerDark.png";
     user.src = "../public/imgs/userDark.png";
     sunMoon.src = "../public/imgs/sun.png";
+    eye.src = "../public/imgs/openEyeDark.png";
   }
 }
 document.addEventListener("DOMContentLoaded", carregarTema);
