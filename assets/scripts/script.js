fec5ac7c4c3dc6bb82ce805aa9cf72ae5249e191
DanielC-007 <brz.danielcarlos@gmail.com>
2024-05-28 10:11:53 -0300

    update srcPasta 05/28 10.10

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@ console.log("Olá, tudo certo");
 
 function navOpenClose() {
   var navSalas = document.getElementById("salas");
-  var bntY = document.getElementById("y");
+  var bntY = document.getElementById("x");
   if (
     navSalas.style.transform === "translateX(-100%)" ||
     navSalas.style.transform === ""
@@ -22,10 +22,10 @@ function exibirSenha() {
   var eye = document.getElementById("eye");
   if (visualizar.type === "password") {
     visualizar.type = "text";
-    eye.src = "../public/imgs/closeEyeLight.png";
+    eye.src = "../src/imgs/closeEyeLight.png";
   } else {
     visualizar.type = "password";
-    eye.src = "../public/imgs/openEyeLight.png";
+    eye.src = "../src/imgs/openEyeLight.png";
   }
 }
 
@@ -38,40 +38,40 @@ function tema() {
 
   if (body.classList.contains("lightTheme")) {
     body.classList.remove("lightTheme");
-    hamburger.src = "../public/imgs/hamburgerDark.png";
-    user.src = "../public/imgs/userDark.png";
-    sunMoon.src = "../public/imgs/sun.png";
-    eye.src = "../public/imgs/openEyeDark.png";
-    localStorage.setItem("theme", "dark");
+    hamburger.src = "../src/imgs/hamburgerDark.png";
+    user.src = "../src/imgs/userDark.png";
+    sunMoon.src = "../src/imgs/sun.png";
+    eye.src = "../src/imgs/openEyeDark.png";
+    // localStorage.setItem("theme", "dark");
   } else {
     body.classList.add("lightTheme");
-    hamburger.src = "../public/imgs/hamburgerLight.png";
-    user.src = "../public/imgs/userLight.png";
-    sunMoon.src = "../public/imgs/moon.png";
-    eye.src = "../public/imgs/openEyeLight.png";
-    localStorage.setItem("theme", "light");
+    hamburger.src = "../src/imgs/hamburgerLight.png";
+    user.src = "../src/imgs/userLight.png";
+    sunMoon.src = "../src/imgs/moon.png";
+    eye.src = "../src/imgs/openEyeLight.png";
+    // localStorage.setItem("theme", "light");
   }
 }
 
-function carregarTema() {
-  const body = document.body;
-  var hamburger = document.getElementById("hamburger");
-  var user = document.getElementById("user");
-  var sunMoon = document.getElementById("sunMoon");
-  var eye = document.getElementById("eye");
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme === "light") {
-    body.classList.add("lightTheme");
-    hamburger.src = "../public/imgs/hamburgerLight.png";
-    user.src = "../public/imgs/userLight.png";
-    sunMoon.src = "../public/imgs/moon.png";
-    eye.src = "../public/imgs/openEyeLight.png";
-  } else {
-    body.classList.remove("lightTheme");
-    hamburger.src = "../public/imgs/hamburgerDark.png";
-    user.src = "../public/imgs/userDark.png";
-    sunMoon.src = "../public/imgs/sun.png";
-    eye.src = "../public/imgs/openEyeDark.png";
-  }
-}
-document.addEventListener("DOMContentLoaded", carregarTema);
+// function carregarTema() {
+//   const body = document.body;
+//   var hamburger = document.getElementById("hamburger");
+//   var user = document.getElementById("user");
+//   var sunMoon = document.getElementById("sunMoon");
+//   var eye = document.getElementById("eye");
+//   const savedTheme = localStorage.getItem("theme");
+//   if (savedTheme === "light") {
+//     body.classList.add("lightTheme");
+//     hamburger.src = "../src/imgs/hamburgerLight.png";
+//     user.src = "../src/imgs/userLight.png";
+//     sunMoon.src = "../src/imgs/moon.png";
+//     eye.src = "../src/imgs/openEyeLight.png";
+//   } else {
+//     body.classList.remove("lightTheme");
+//     hamburger.src = "../src/imgs/hamburgerDark.png";
+//     user.src = "../src/imgs/userDark.png";
+//     sunMoon.src = "../src/imgs/sun.png";
+//     eye.src = "../src/imgs/openEyeDark.png";
+//   }
+// }
+// document.addEventListener("DOMContentLoaded", carregarTema);
