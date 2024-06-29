b4aa422599bf253738a9c0081105ddba304e6487
MrMeme777 <brz.danielcarlos@gmail.com>
2024-06-29 10:20:54 -0300

    update database

--- database/database.sql
+++ database/database.sql
@@ -1,25 +1,29 @@
 create database conexao;
 use conexao;
 
-create table entrada(
-codigo_usuario int NOT NULL AUTO_INCREMENT,
-nome_usuario varchar(100),
-senha_usuario varchar(100),
-email_usuario varchar(100)
+create table info_user(
+id_user int NOT NULL AUTO_INCREMENT,
+nome varchar(9999),
+apelido varchar(9999),
+senha varchar(9999),
+email varchar(9999),
+RM int(5),
+primary key (id_user)
 );
 
-create table posts(
-codigo_Complementares int NOT NULL AUTO_INCREMENT,
-codigo_usuario int NOT NULL,
-publicacao_usuario varchar(100),
-comentarios_usuario varchar(200)
+create table post_user(
+id_post int not null auto_increment,
+descricao varchar(9999),
+curtidas_post int,
+primary key (id_post)
 );
 
-create table chats(
-codigo_salas int NOT NULL AUTO_INCREMENT,
-codigo_usuario int NOT NULL,
-mensagens varchar(200)
+create table comentarios(
+id_coment int not null auto_increment,
+comentario_user varchar(9999),
+curtidas_coment int,
+primary key (id_coment),
+foreign key(id_user) references info_user(id_user),
+foreign key(id_post) references post_user(id_post)
 );
 
-/* INSERT INTO entrada (codigo_usuario, nome_usuario, senha_usuario, email_usuario) 
-VALUES (1, 'Maísa', 'Maísa123$', 'maisa.souza32@etec.sp.gov.br'); */
