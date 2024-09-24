8310ccd508ff20e3161233b6ad3d8c87be5fe3ba
DanielC-007 <brz.danielcarlos@gmail.com>
2024-09-24 11:22:32 -0300

    conn update

--- connection/connect.php
+++ connection/connect.php
@@ -1,26 +1,22 @@
 <?php
+// $servidor = "localhost";
+// $usuario = "root";
+// $senha = "";
+// $banco = "conexaoTecfor";
+// $connect = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+// if(mysqli_connect_errno()){
+//     die("Conexão falhou" . mysqli_connect_errno());
+// }
+
 $servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "conexaoTecfor";
+$usuario = "u774820395_DanielCarlos";
+$senha = "Segunda@10";
+$banco = "u774820395_conexaoTecFor";
 $connect = mysqli_connect($servidor, $usuario, $senha, $banco);
 
 if(mysqli_connect_errno()){
     die("Conexão falhou" . mysqli_connect_errno());
 }
-
-// $nome = $_POST['nameUser'];
-// $email = $_POST['emailUser'];
-// $senha = $_POST['passUser'];
-
-// $sql = "INSERT INTO entrada (codigo_usuario, nome_usuario, senha_usuario, email_usuario) VALUES ('$nome', '$email', '$senha')";
-
-// if (mysqli_query($conn, $sql)) {
-//     echo "Dados inseridos com sucesso";
-// } else {
-//     echo "Erro ao inserir dados: " . mysqli_error($conn);
-// }
-
-// mysqli_close($conn);
 
 ?>
