6c079d1cd1c286017214b9b36ebe152be869a851
DanielC-007 <brz.danielcarlos@gmail.com>
2024-10-08 11:50:07 -0300

    filter

--- pages/sala1.php
+++ pages/sala1.php
@@ -1,3 +1,14 @@
+<?php
+  session_start();
+  include_once('../connection/connect.php');
+  if(!isset($_SESSION['email']) == true and !isset($_SESSION['senha']) == true){
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header('Location: login.php');
+    }
+    $logado = $_SESSION['email'];
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
