0d834dfd16cc21eb5ae0456e591f200023ff8b9d
MrMeme777 <brz.danielcarlos@gmail.com>
2024-11-13 19:24:55 -0300

    style comentario beta

--- posts/arquivos_publicados.php
+++ posts/arquivos_publicados.php
@@ -56,19 +56,23 @@ $sql_query = $connect->query("
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Arquivos Publicados</title>
 </head>
+
 <body>
     <?php while ($arquivo = $sql_query->fetch_assoc()) { ?>
-        <div class="publicacaoDiv">
-            <img src="../posts/<?php echo $arquivo['path']; ?>">
-            <div>
+    <div class="publicacaoDiv">
+        <img src="../posts/<?php echo $arquivo['path']; ?>">
+        <div>
+            <div class="caixa2">
                 <div class="postInfo">
-                    <img class="imgUser" src="<?php echo htmlspecialchars($arquivo['foto_perfil']) ? htmlspecialchars($arquivo['foto_perfil']) : '../src/imgs/userDefault.png'; ?>">
+                    <img class="imgUser"
+                        src="<?php echo htmlspecialchars($arquivo['foto_perfil']) ? htmlspecialchars($arquivo['foto_perfil']) : '../src/imgs/userDefault.png'; ?>">
                     <div>
                         <h4><?php echo htmlspecialchars($arquivo['nome_aluno']); ?></h4>
                         <h6><?php echo date("d/m/Y", strtotime($arquivo['data_uploaded'])); ?></h6>
@@ -79,46 +83,50 @@ $sql_query = $connect->query("
                     <h1><?php echo htmlspecialchars($arquivo['titulo']); ?></h1>
                     <p><?php echo htmlspecialchars($arquivo['comentario']); ?></p>
                 </div>
-
-                <!-- Comentários -->
                 <div class="comentarios">
                     <?php
-                    // Consulta para obter os comentários do arquivo atual
-                    $comentarios_stmt = $connect->prepare("
-                        SELECT comentarios.*, alunos.nome AS nome_aluno, alunos.foto_perfil
-                        FROM comentarios
-                        JOIN alunos ON comentarios.id_aluno = alunos.id_aluno
-                        WHERE comentarios.id_arquivo = ?
-                        ORDER BY comentarios.data_comentario DESC
-                    ");
-                    $comentarios_stmt->bind_param("i", $arquivo['id']);
-                    $comentarios_stmt->execute();
-                    $comentarios_result = $comentarios_stmt->get_result();
-
-                    while ($comentario = $comentarios_result->fetch_assoc()) { ?>
-                        <div class="comentario">
-                            <img class="imgUser" src="<?php echo htmlspecialchars($comentario['foto_perfil']) ? htmlspecialchars($comentario['foto_perfil']) : '../src/imgs/userDefault.png'; ?>">
+                            // Consulta para obter os comentários do arquivo atual
+                            $comentarios_stmt = $connect->prepare("
+                                SELECT comentarios.*, alunos.nome AS nome_aluno, alunos.foto_perfil
+                                FROM comentarios
+                                JOIN alunos ON comentarios.id_aluno = alunos.id_aluno
+                                WHERE comentarios.id_arquivo = ?
+                                ORDER BY comentarios.data_comentario DESC
+                            ");
+                            $comentarios_stmt->bind_param("i", $arquivo['id']);
+                            $comentarios_stmt->execute();
+                            $comentarios_result = $comentarios_stmt->get_result();
+        
+                            while ($comentario = $comentarios_result->fetch_assoc()) { ?>
+                    <div class="comentario">
+                        <div>
+                            <img class="imgUser"
+                                src="<?php echo htmlspecialchars($comentario['foto_perfil']) ? htmlspecialchars($comentario['foto_perfil']) : '../src/imgs/userDefault.png'; ?>">
                             <div>
                                 <h4><?php echo htmlspecialchars($comentario['nome_aluno']); ?></h4>
-                                <p><?php echo htmlspecialchars($comentario['comentario']); ?></p>
-                                <small><?php echo date("d/m/Y H:i", strtotime($comentario['data_comentario'])); ?></small>
+                                <h6><?php echo date("d/m/Y", strtotime($comentario['data_comentario'])); ?></>
                             </div>
                         </div>
+                        <p><?php echo htmlspecialchars($comentario['comentario']); ?></p>
+                    </div>
                     <?php }
-                    $comentarios_stmt->close();
-                    ?>
+                            $comentarios_stmt->close();
+                            ?>
                 </div>
-
-                <!-- Formulário de comentário -->
-                <div class="comentarios">
+            </div>
+            <div class="caixa2">
+                <div class="comentar">
                     <form action="" method="POST">
                         <input type="hidden" name="id_arquivo" value="<?php echo $arquivo['id']; ?>">
-                        <textarea style="color: black;" name="comentario" placeholder="Escreva um comentário..." required></textarea>
+                        <textarea style="color: black;" name="comentario" placeholder="Escreva um comentário..."
+                            required></textarea>
                         <button type="submit">Comentar</button>
                     </form>
                 </div>
             </div>
         </div>
+    </div>
     <?php } ?>
 </body>
+
 </html>
