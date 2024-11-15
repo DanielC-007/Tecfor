bcfda3a026c4b49ac2944dccd12a39311c615d44
MrMeme777 <brz.danielcarlos@gmail.com>
2024-11-15 11:02:35 -0300

    update

--- posts/arquivos_publicados.php
+++ posts/arquivos_publicados.php
@@ -84,20 +84,21 @@ $sql_query = $connect->query("
                     <p><?php echo htmlspecialchars($arquivo['comentario']); ?></p>
                 </div>
                 <div class="comentarios">
+                    <h3>Comentários</h3>
                     <?php
-                            // Consulta para obter os comentários do arquivo atual
-                            $comentarios_stmt = $connect->prepare("
-                                SELECT comentarios.*, alunos.nome AS nome_aluno, alunos.foto_perfil
-                                FROM comentarios
-                                JOIN alunos ON comentarios.id_aluno = alunos.id_aluno
-                                WHERE comentarios.id_arquivo = ?
-                                ORDER BY comentarios.data_comentario DESC
-                            ");
-                            $comentarios_stmt->bind_param("i", $arquivo['id']);
-                            $comentarios_stmt->execute();
-                            $comentarios_result = $comentarios_stmt->get_result();
-        
-                            while ($comentario = $comentarios_result->fetch_assoc()) { ?>
+                                // Consulta para obter os comentários do arquivo atual
+                                $comentarios_stmt = $connect->prepare("
+                                    SELECT comentarios.*, alunos.nome AS nome_aluno, alunos.foto_perfil
+                                    FROM comentarios
+                                    JOIN alunos ON comentarios.id_aluno = alunos.id_aluno
+                                    WHERE comentarios.id_arquivo = ?
+                                    ORDER BY comentarios.data_comentario DESC
+                                ");
+                                $comentarios_stmt->bind_param("i", $arquivo['id']);
+                                $comentarios_stmt->execute();
+                                $comentarios_result = $comentarios_stmt->get_result();
+            
+                                while ($comentario = $comentarios_result->fetch_assoc()) { ?>
                     <div class="comentario">
                         <div>
                             <img class="imgUser"
@@ -107,19 +108,23 @@ $sql_query = $connect->query("
                                 <h6><?php echo date("d/m/Y", strtotime($comentario['data_comentario'])); ?></>
                             </div>
                         </div>
-                        <p><?php echo htmlspecialchars($comentario['comentario']); ?></p>
+                        <p><?php echo nl2br(wordwrap(htmlspecialchars($comentario['comentario']), 28, "\n", true)); ?>
+                        </p>
                     </div>
                     <?php }
-                            $comentarios_stmt->close();
-                            ?>
+                        $comentarios_stmt->close();
+                        ?>
                 </div>
             </div>
             <div class="caixa2">
                 <div class="comentar">
                     <form action="" method="POST">
+                        <!-- <button class="like-button" id="likeBtn">
+                            <span class="heart empty"><img src="../src/imgs/heartNull.png"></span>
+                            <span class="heart filled"><img src="../src/imgs/heartCheio.png"></span>
+                        </button> -->
                         <input type="hidden" name="id_arquivo" value="<?php echo $arquivo['id']; ?>">
-                        <textarea style="color: black;" name="comentario" placeholder="Escreva um comentário..."
-                            required></textarea>
+                        <textarea name="comentario" placeholder="Escreva um comentário..." required></textarea>
                         <button type="submit">Comentar</button>
                     </form>
                 </div>
@@ -130,3 +135,25 @@ $sql_query = $connect->query("
 </body>
 
 </html>
+
+<script>
+const likeBtn = document.getElementById('likeBtn');
+const emptyHeart = document.querySelector('.empty');
+const filledHeart = document.querySelector('.filled');
+
+likeBtn.addEventListener('click', function() {
+    this.classList.add('animate');
+    setTimeout(() => {
+        this.classList.remove('animate');
+    }, 600); // Duração da animação
+
+    // Alternar a visibilidade dos corações
+    if (emptyHeart.style.opacity === '0') {
+        emptyHeart.style.opacity = '1';
+        filledHeart.style.opacity = '0';
+    } else {
+        emptyHeart.style.opacity = '0';
+        filledHeart.style.opacity = '1';
+    }
+});
+</script>
